42f48e752920cad4efb5cd1bb16f1df868ccfcb4
ben vandenbosch <benjaminrvandenbosch@gmail.com>
2018-12-04 21:37:53 -0500

    fix model path issue

--- traffic-lights/cnn_traffic_light.py
+++ traffic-lights/cnn_traffic_light.py
@@ -27,11 +27,9 @@ def cnn_model_fn(features, labels, mode):
 
   # With "valid" padding, this should output a
   # [-1, 40, 40, 10]
-  print("conv1")
   # Pooling Layer #1
   pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
   # [-1, 20, 20, 10]
-  print("pool1")
   # Convolutional Layer #2 and Pooling Layer #2
   conv2 = tf.layers.conv2d(
       inputs=pool1,
@@ -40,23 +38,17 @@ def cnn_model_fn(features, labels, mode):
       padding="valid",
       activation=tf.nn.relu)
 
-  print("conv2")
   # With "valid" padding, should output:
   # [-1, 10, 10, 20]
   pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
-  print("pool2")
   #[-1, 5, 5, 20]
   # Dense Layer
   pool2_flat = tf.reshape(pool2, [-1, 5 * 5 * 20])
-  print("pool2flat")
   dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
-  print("dense")
   dropout = tf.layers.dropout(
       inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
-  print("dropout")
   # Logits Layer
   logits = tf.layers.dense(inputs=dropout, units=3)
-  print("logits")
   predictions = {
       # Generate predictions (for PREDICT and EVAL mode)
       "classes": tf.argmax(input=logits, axis=1),
@@ -117,7 +109,6 @@ def parser(record):
   image = tf.cast(image, tf.float32)
   #image = tf.reshape(image, shape=[224, 224, 3])
   label = tf.cast(parsed["label"], tf.int32)
-  print(image, label)
   return {'x': image}, label
 
 
@@ -131,7 +122,7 @@ eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
 
 # Create the Estimator
 mnist_classifier = tf.estimator.Estimator(
-    model_fn=cnn_model_fn, model_dir="/tmp/whydoihavetokeepchangingthis")
+    model_fn=cnn_model_fn, model_dir="model/test")
 
 # Set up logging for predictions
 tensors_to_log = {"probabilities": "softmax_tensor"}
@@ -157,7 +148,7 @@ logging_hook = tf.train.LoggingTensorHook(
 #   num_epochs=1,
 #   shuffle=False)
 
-mnist_classifier.train(input_fn=train_input_fn, steps=3000)
+mnist_classifier.train(input_fn=train_input_fn, steps=1000)
 eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
 print(eval_results)
 # if __name__ == "__main__":
